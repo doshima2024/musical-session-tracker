b8ffb64a1370ac17f684327979a3908459ed8e44
Daniel Oshima <doshima2013@gmail.com>
2026-01-30 19:25:00 -0500

    Scaffold and initialize Flask app, imports and test server

---
+++ server/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_cors import CORS
+from extensions import db
+
+app = Flask(__name__) # initialize Flask app instance
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JSON_COMPACT"] = False 
+
+db.init_app(app) 
+
+@app.route('/')
+def home():
+    return "Server run success."
+    
+
+if app.name == '__main__':
+    app.run(debug=True)
