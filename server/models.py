22b49622594f3c72b732abdd3aef901cd3fe4b12
Daniel Oshima <doshima2013@gmail.com>
2026-02-02 10:18:01 -0500

    add __repr__ methods for Session and MusicalIdea Models

--- server/models.py
+++ server/models.py
@@ -7,9 +7,14 @@ class Session(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     length = db.Column(db.Integer)
+    notes = db.Column(db.Text)
     started_at = db.Column(db.String, nullable=False)
 
     musical_ideas = db.relationship("MusicalIdea", back_populates="session", cascade = "all, delete-orphan")
+
+    def __repr__(self):
+        return f"Session id: {self.id} title: {self.title} length: {self.length} started_at: {self.started_at}"
+        
  
 class MusicalIdea(db.Model):
     __tablename__="musical_ideas"
@@ -23,6 +28,7 @@ class MusicalIdea(db.Model):
 
     session = db.relationship("Session", back_populates="musical_ideas")
 
-
+    def __repr__(self):
+        return f"MusicalIdea id: {self.id} title {self.title} bpm: {self.bpm} key: {self.key} session_id: {self.session_id} "
 
 
