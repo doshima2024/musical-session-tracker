52568911206b6959ec3a879874a7eecebd6f41d0
Daniel Oshima <doshima2013@gmail.com>
2026-02-04 14:09:07 -0500

    add GET /sessions/<int:id> route

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_migrate import Migrate
 from flask_cors import CORS
 from extensions import db
@@ -17,7 +17,31 @@ migrate = Migrate(app, db)
 @app.route('/')
 def home():
     return "Server run success."
-    
+
+# GET route to return all sessions
+
+@app.get('/sessions')
+def retrieve_sessions():
+    try:
+        sessions = Session.query.all()
+        return jsonify([session.to_dict() for session in sessions]), 200
+    except Exception as exception:
+        app.logger.exception("GET /sessions failed")
+        return jsonify({"error": str(exception)}), 500
+
+# GET route to return a specific session, by id
+
+@app.get('/sessions/<int:id>')
+def retrieve_session(id):
+    session = Session.query.filter(Session.id == id).first()
+    if session is None:
+        return jsonify({'error': 'session with that ID not found'}), 404
+    try:
+        return jsonify(session.to_dict()), 200
+    except Exception as exception:
+        app.logger.exception('GET /sessions/<int:id> failed')
+        return jsonify({'error': str(exception)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
