e2b6f227ccc2fd3836307a37f440307643b17be4
Daniel Oshima <doshima2013@gmail.com>
2026-03-04 12:29:19 -0500

    remove unused imports in MusicalIdeaList

--- client/src/components/MusicalIdeaList.jsx
+++ client/src/components/MusicalIdeaList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { MusicalIdeaDisplayCard } from './MusicalIdeaDisplayCard';
 
 export const MusicalIdeaList = ({ ideas, onIdeaDelete }) => {
